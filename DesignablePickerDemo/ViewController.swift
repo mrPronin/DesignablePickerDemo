27bed6da591aa90033477094e86873023b3d150d
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2017-09-25 10:27:31 +0200

    Initial Commit

---
+++ DesignablePickerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignablePickerDemo
+//
+//  Created by Oleksandr Pronin on 25.09.17.
+//  Copyright © 2017 adviqo AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
